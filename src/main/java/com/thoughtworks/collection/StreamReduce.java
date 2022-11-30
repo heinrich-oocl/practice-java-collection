6b05cc56cca1b910dcdf4d4b9cc14694251655d0
heinrich-oocl <117629815+heinrich-oocl@users.noreply.github.com>
2022-11-30 14:55:47 +0800

    feat: implement all methods

--- src/main/java/com/thoughtworks/collection/StreamReduce.java
+++ src/main/java/com/thoughtworks/collection/StreamReduce.java
@@ -5,14 +5,17 @@ import java.util.List;
 public class StreamReduce {
 
     public int getLastOdd(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+                .reduce((a,b) -> b%2==0 ? a : b)
+                .get();
     }
 
     public String getLongest(List<String> words) {
-        return null;
+        return words.stream()
+                .reduce(words.get(0), (a,b) -> b.length()>a.length() ? b : a);
     }
 
     public int getTotalLength(List<String> words) {
-        return 0;
+        return words.stream().reduce(0, (a, b) ->a + b.length(), Integer::sum);
     }
 }
